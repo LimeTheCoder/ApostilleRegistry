82fd0f3b5ef725f39da6e4028ea173ff0cc330e2
Sergiy <s.prihodko@bytesarea.com.ua>
2016-03-03 00:48:49 +0200

    Add 'Actor' model

--- src/apostille/settings.py
+++ src/apostille/settings.py
@@ -31,12 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'register.apps.RegisterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'files', 'media')
+MEDIA_URL = '/media/'
---
+++ src/register/models.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Actor(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=50)
+    patronymic = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    job_start_date = models.DateTimeField('Job start date')
+    job_finish_date = models.DateTimeField('Job finish date')
+    sign = models.ImageField()
+    stamp = models.ImageField()
+
+

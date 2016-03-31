9e817d67df4a175b0616f35d4c88f4bfd17f8f63
LimeTheCoder <limethecoder@gmail.com>
2016-03-31 13:19:07 +0300

    Add minor changes in tests

--- src/register/tests.py
+++ src/register/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Organ
 
 # Create your tests here.
+
+
+class OrganModelTest(TestCase):
+
+    def test_string_representation(self):
+        organ = Organ(name='Judge')
+        self.assertEqual(str(organ), organ.name)

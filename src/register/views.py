ae4762806c93782f853dd5d1cf1f542af7971485
LimeTheCoder <limethecoder@gmail.com>
2016-05-18 02:34:33 +0300

    Add export apostille to pdf

--- src/register/views.py
+++ src/register/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import get_object_or_404, render, redirect
+
+from django.template.loader import get_template
+from django.template import Context
 from django.http import HttpResponse
 
 from .models import Apostille
 
 import datetime
+import pdfkit
 
 def search(request):
 	if request.method == 'POST':
@@ -24,10 +28,39 @@ def search(request):
 
 
 def apostille_detail(request, id):
-	if not request.session.get('form-submitted', False):
-		return HttpResponse("No such page")
+	#if not request.session.get('form-submitted', False):
+	#	return HttpResponse("No such page")
+
 
 	apostille = get_object_or_404(Apostille, pk=id)
-	context = {'apostille' : apostille }
-	request.session['form-submitted'] = False
+	context = {'apostille' : apostille}
+
+	if 'to_pdf_btn' in request.GET:
+		context['export_mode'] = True
+		return render_to_pdf('register/apostille_detail.html', context)
+
+	context['export_mode'] = False
+	#request.session['form-submitted'] = False
 	return render(request, 'register/apostille_detail.html', context)
+
+
+def render_to_pdf(template_src, context_dict):
+    template = get_template(template_src)
+    context = Context(context_dict)
+    html  = template.render(context)
+    
+    options = {
+    'page-size': 'Letter',
+    'margin-top': '0.75in',
+    'margin-right': '0.75in',
+    'margin-bottom': '0.75in',
+    'margin-left': '0.75in',
+    'encoding': "UTF-8"
+}
+
+    pdf = pdfkit.from_string(html, False, options=options)
+    
+    response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename=output.pdf'
+    
+    return response
